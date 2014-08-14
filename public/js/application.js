9ea71dc13dd7f6e1287542264b58fea68fd60c19
Katia "Bam Bam" Basma & Lauren <pair+kbasma.lkroner@devbootcamp.com>
2014-08-14 12:44:38 -0700

    Add click event to tabble cell.

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,10 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+$("td").click(function() {
+  elem = document.createElement("div");
+  elem.className = "poop";
+  if ($(this).find(".poop").length == 0) {
+    $(this).append( elem );
+  };
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
+
