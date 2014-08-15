0962c5bd7fdc51c87d693072aa246882c356568a
Katia "Bam Bam" Basma <katiam2894@gmail.com>
2014-08-15 15:02:03 -0700

    checks 4 winners

--- public/js/application.js
+++ public/js/application.js
@@ -11,6 +11,10 @@ $("td").click(function() {
     cellToFill.append( playerPic );
   }
   checkCols();
+  checkRows();
+  for (var i = -2 ; i <= 3; i++){
+    checkDiag(i)
+  }
 });
 
 var findCellToFill = function(columnCells) {
@@ -46,40 +50,115 @@ var togglePlayer = function() {
 };
 
 var checkCols = function() {
-  // console.log("in checkCols")
-  var player1Counter = 0;
-  var player2Counter = 0;
   for (var column = 1; column <= 7; column ++){
-    // console.log("inside first column for loop")
-    var columnCells = $("." + column);
+    var player1Counter = 0;
+    var player2Counter = 0;
+    var columnCells = $(".col_" + column);
+
     for (var index = 0; index <= 5; index ++) {
-      // console.log("inside cell for loop")
       var cell = $(columnCells[index])
-      cell.find(".player1").length
       if (cell.find(".player1").length >= 1){
-        player1Counter += 1
-        player2Counter = 0 
-        console.log("player1" + player1Counter)
+        player1Counter += 1;
+        player2Counter = 0 ;
       } else if (cell.find(".player2").length >= 1) {
-        player2Counter += 1
-        player1Counter = 0 
-        console.log("player2" + player2Counter)
-      }
+        player2Counter += 1;
+        player1Counter = 0 ;
+      } 
+    }
+    if (player1Counter >=4 || player2Counter >= 4) {
+      alert("You have a winner!");
+      location.reload();
     }
   }
-  if (player1Counter >=4 || player2Counter >= 4) {
-    alert("winnnerrrr")
+
+}
+
+var checkRows = function() {
+  for (var row = 1; row <= 7; row ++){
+    var player1Counter = 0;
+    var player2Counter = 0;
+    var rowCells = $($("#row_"+row).children());
+    for (var index = 0; index <= 5; index ++) {
+      var cell = $(rowCells[index])
+      if (cell.find(".player1").length >= 1){
+        player1Counter += 1;
+        player2Counter = 0 ;
+      } else if (cell.find(".player2").length >= 1) {
+        player2Counter += 1;
+        player1Counter = 0;
+      } 
+    }
+    if (player1Counter >=4 || player2Counter >= 4) {
+      alert("You have a winner!");
+      location.reload();
+    }
+  }
+
+}
+
+var checkDiag = function(difference) {
+  console.log(difference)
+  var player1Counter = 0;
+  var player2Counter = 0;
+  for (var row = 1; row <= 7; row ++){
+    colIndex = row + difference - 1;
+    var rowCells = $($("#row_"+row).children());
+    if ($($(rowCells[colIndex])).find(".player1").length == 1) {
+      player1Counter += 1;
+      player2Counter = 0;
+    } else if ($($(rowCells[colIndex])).find(".player2").length == 1) {
+      player2Counter += 1;
+      player1Counter = 0;
+    }
+
+    if (player1Counter >=4 || player2Counter >= 4) {
+      alert("You have a winner!");
+      location.reload();
+    }
+  }
+  for (var row = 7; row <= 1; row --){
+    colIndex = row + difference - 1;
+    var rowCells = $($("#row_"+row).children());
+    if ($($(rowCells[colIndex])).find(".player1").length == 1) {
+      player1Counter += 1;
+      player2Counter = 0;
+    } else if ($($(rowCells[colIndex])).find(".player2").length == 1) {
+      player2Counter += 1;
+      player1Counter = 0;
+    }
+
+    if (player1Counter >=4 || player2Counter >= 4) {
+      alert("You have a winner!");
+      location.reload();
+    }
   }
 }
 
 
 
 
+//Make color change
 
-// var check_rows_for_winner = function() {
-//   for (var i = 1; i <= 6; i++) {
-//     rowCells = $("row_" + i);
+var makeBlue = function() {
+  $(this).css('background-color', '#007fff')
+}
 
-//   }
+var makeGreen = function() {
+  $(this).css('background-color', '#008000')
+}
 
-// };
+var makeWhite= function() {
+  $(this).css('background-color', 'white')
+}
+
+if (window.playerId === 1) {
+  // var $(column) = ('td.').attr('class')
+  $('td').on({mouseover: makeBlue, mouseout: makeWhite})
+}
+else if (window.playerId === 2) {
+  $('td').off({mouseover: makeBlue, mouseout: makeWhite})
+  $('td').on({mouseover: makeGreen, mouseout: makeWhite})
+}
+
+
+
